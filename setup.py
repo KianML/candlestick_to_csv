ac0502f47438e329d650f59a6b56ae4b055f6159
Kianoosh Keshavarzian <kshvzn@gmail.com>
2025-06-03 09:59:05 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+from src.candlestick_to_csv import __version__
+
+setup(
+    name='candlestick_to_csv',
+    version=__version__,
+
+    url='https://github.com/KianML/candlestick_to_csv',
+    author='Kianoosh Keshavarzian',
+    author_email='kshvzn@gmail.com',
+)
